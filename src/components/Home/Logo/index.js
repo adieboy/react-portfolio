226f8c5f42c65cbac7c53d8e28f76f441ff1767c
Adie <capilitanadie@gmail.com>
2023-08-11 01:47:13 +0800

    lplplp

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -17,6 +17,20 @@ const Logo = () => {
       duration: 1,
       opacity: 1,
     })
+    .from(outlineLogoRef.current, {
+      drawSVG: 0,
+      duration: 20,
+    })
+    gsap.fromTo(
+      solidLogoRef.current,{
+        opacity: 0,
+      }, {
+        opacity:1,
+        delay: 4,
+        duration: 4,
+      }
+      
+    )
   },[])
 
   return (
